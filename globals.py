4619f87ba8e0af8e4c47dca2c2b403a23319b0d3
DanielGascaGarcia <daniel.gasca.garcia.1@gmail.com>
2025-11-20 19:28:03 +0000

    Replace IDs and paths with placeholder values
    Updated global variables to placeholder values.

--- globals.py
+++ globals.py
@@ -1,13 +1,5 @@
 
-# id = "adolescent#007"
-# id = "child#03"
-# id = "adult#001"
-# id="Patient_20"
-
-id = 2308;
-# id2="Patient_5";
-
-id = 588;
-path1='C:/OhioDataset/ExploratoryAnalysisData/OhioT1DM/2018/train/';
-path2='C:/OhioDataset/ExploratoryAnalysisData/OhioT1DM/2018/parsedTexts/';
+id = "SET_ME"
+path1="SET_ME"
+path2="SET_ME"
 
